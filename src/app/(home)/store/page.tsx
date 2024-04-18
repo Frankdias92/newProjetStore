1f1c94d984ccbc45d147d710415ff837a6d288bc
Franklin Macedo Dias <frankmcdias@gmail.com>
2024-04-18 15:33:25 -0300

    feat: add page product list

--- src/app/(home)/store/page.tsx
+++ src/app/(home)/store/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import { ProductList } from "@/components/store/productLists"
-import {Tabs, Tab} from "@nextui-org/react"
+import {Tabs, Tab, Card, CardBody} from "@nextui-org/react"
 
 
 export default function Store() {
     const variants = [ "underlined"]
+
     
 
     return (
@@ -13,7 +14,18 @@ export default function Store() {
             <div className="flex flex-col w-3/4 h-screen  m-auto">
 
                 {variants.map((variant) => (
-                    <Tabs key={variant} variant={'underlined'} aria-label="Tabs variants">
+                    <Tabs 
+                        key={variant} 
+                        variant={'underlined'} 
+                        aria-label="Tabs variants"
+                    >
+                        <Tab key="Todos" title="Todos">
+                            <Card>
+                                <CardBody>
+                                    <ProductList />
+                                </CardBody>
+                            </Card>  
+                        </Tab>
                         <Tab key="Brasil" title="Brasil"/>
                         <Tab key="Logitech" title="Logitech"/>
                         <Tab key="Thrustmaster" title="Thrustmaster"/>
@@ -23,7 +35,10 @@ export default function Store() {
                     </Tabs>
                 ))}
                 
-                <ProductList />
+
+                <div>
+
+                </div>
             </div>
         </section>
     )
--- src/components/store/productLists.tsx
+++ src/components/store/productLists.tsx
@@ -1,10 +1,83 @@
+import {Button, Image, ScrollShadow} from "@nextui-org/react"
 
 
-export function ProductList() {
+export function ProductList({variants}: any) {
+    
+    const dataFeature = [
+        {
+            id: 1,
+            productName: 'Logitech G29',
+            productDescriptions: 'Melhor volante de entrada pra quem procura algo novo e com garantia. O volante tem um Force Feedback que já dá uma boa noção de como o carro se comporta e os pedais tem embreagem',
+            price: 'R$ 1817,05',
+            image: 'https://m.media-amazon.com/images/I/61j1ZCgzQRL._AC_SL1500_.jpg'
+        },
+        {
+            id: 2,
+            productName: ' Thrustmaster T300 RS GT Edition',
+            productDescriptions: 'O volante Force Feedback Thrustmaster T300RS GT permite aos jogadores mergulharem verdadeiramente no mundo das corridas de automóveis e aperfeiçoarem as suas técnicas para melhorarem o seu desempenho.',
+            price: 'R$ 3.299,90',
+            image: 'https://images.kabum.com.br/produtos/fotos/150106/volante-e-pedais-thrustmaster-t300-rs-gt-edition-para-pc-ps4-ps3-4160644_1616590119_gg.jpg'
+        },
+        {
+            id: 3,
+            productName: ' Thrustmaster T300 RS GT Edition',
+            productDescriptions: 'O volante Force Feedback Thrustmaster T300RS GT permite aos jogadores mergulharem verdadeiramente no mundo das corridas de automóveis e aperfeiçoarem as suas técnicas para melhorarem o seu desempenho.',
+            price: 'R$ 3.299,90',
+            image: 'https://images.kabum.com.br/produtos/fotos/150106/volante-e-pedais-thrustmaster-t300-rs-gt-edition-para-pc-ps4-ps3-4160644_1616590119_gg.jpg'
+        }
+    ]
+    
 
     return (
-        <div>
-            <h1>product list</h1>
+
+
+        <div className="flex w-full gap-5">
+
+            {dataFeature.map((item) => {
+                return (
+
+                    <div key={item.id} className="flex flex-col items-center w-[393px] gap-4 h-[509px] px-6 py-5 rounded-3xl hover:bg-neutral-950/60 duration-75 relative">
+                            <Image
+                                isZoomed
+                                width={345}
+                                alt={item.productName}
+                                src={item.image}
+                                className="flex w-[345px] h-[171px]"
+                            />
+
+
+                        <div className="flex flex-col gap-3 items-start">
+                            <span className="text-4xl font-galantic">
+                                {item.productName}
+                            </span>
+                            <span className="text-lg font-roboto">
+                                <ScrollShadow 
+                                    hideScrollBar 
+                                    offset={100}
+                                    orientation="horizontal" 
+                                    className="max-w-[393px] max-h-[135px]"
+                                >
+                                    {item.productDescriptions}
+                                </ScrollShadow>
+                            </span>
+                            <span className="flex w-full  px-5 justify-between items-center font-galantic tracking-widest text-2xl absolute bottom-5 left-0">
+                                {item.price}
+
+                                <Button className="flex py-2 px-5 font-roboto text-sm font-bold bg-store-orange">
+                                    COMPRAR
+                                </Button>
+
+                            </span>
+                        </div>
+
+                    </div>
+                    
+                )
+            })}
+            
+
+
+            
         </div>
     )
 }
